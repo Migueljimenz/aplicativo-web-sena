c37cf6a03ec5f2bde40fc9defdd0183baaaa82e9
Migueljimenz <migueljiarteta@gmail.com>
2022-04-03 15:06:18 -0500

    Merge branch 'main' of https://github.com/Migueljimenz/aplicativo-web-sena into main

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,14 +2,14 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { host: "https://aplicativowebsena.herokuapp.com/users/password/new" }
+  config.action_mailer.default_url_options = { host: "aplicativowe.herokuapp.com", protocol: "https" }
   config.action_mailer.perform_deliveries = true
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'https://aplicativowebsena.herokuapp.com/users/password/new',
+    domain:               'https://aplicativowebsena.herokuapp.com/users/sign_in',
     user_name:            Rails.application.credentials.dig(:google_smtp, :email),
     password:             Rails.application.credentials.dig(:google_smtp, :password),
     authentication:       'plain',
